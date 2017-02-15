5d2d8aef5b9177628e1d21f4e6a18d71fd77b6e7
iAmGoldenboy <miklas@miklasnjor.com>
2017-02-15 09:15:29 +0100

    start up

---
+++ whos_news.py
@@ -0,0 +1,17 @@
+__author__ = 'Miklas Njor - iAmGoldenboy - http://miklasnjor.com'
+__projectname__ = 'whos_news / whos_news.py'
+__datum__ = '15/02/17'
+
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='static')
+
+@app.route("/")
+def home():
+    salute = "hej there"
+    return render_template("base.html", salute=salute)
+
+
+if __name__ == "__main__":
+
+    app.run()
