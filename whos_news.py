79c91dc0f18d59e8f8ce544ddafef80a0fddaf79
iAmGoldenboy <miklas@miklasnjor.com>
2017-02-15 09:54:08 +0100

    small changes

--- whos_news.py
+++ whos_news.py
@@ -3,14 +3,21 @@ __projectname__ = 'whos_news / whos_news.py'
 __datum__ = '15/02/17'
 
 from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def home():
     salute = "hej there"
+    articleLink = "http://politiken.dk/indland/politik/art5833361/Borgmestre-er-åbne-for-at-rykke-politikere-til-provinsen"
+    getLinkData = requests.get(articleLink)
+    soup = BeautifulSoup(getLinkData.content, "lxml")
+    print("original encoding", soup.original_encoding, soup )
+    # alltext =
     val = "dfdf"
-    return render_template("base.html", salute=salute)
+    return render_template("base.html", salute=salute, articleLink=articleLink, soup=soup)
 
 
 if __name__ == "__main__":
